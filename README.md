cf9874f857a6a74c92e38dec9cdec4a6a1cfe3cc
mokunine <develop.kidoguchi@gmail.com>
2022-08-04 23:42:07 +0900

    [add] First

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# patlite_phe-3fb2_sample
-PATLITE PHE-32FB2 sample
+# 概要
+
+PATLITE製インターフェースコンバータ[PHE-3FB2](https://www.patlite.jp/product/phe_3fb2.html) のサンプルプログラム。
+
+# 使用モジュール
+
+## argparse
+
+UNIXライクのコマンドラインオプションパーサー。Python標準ライブラリ。
+
+## configparser
+
+INIファイルから値を読み込む。Python標準ライブラリ。
+
+## PySerial
+
+シリアル通信モジュール。詳細は[公式サイト](https://pyserial.readthedocs.io/en/latest/)を参照。
+
+## PyInstaller
+
+pythonスクリプトをexe化するモジュール。詳細は[公式サイト](https://pyinstaller.org/en/stable/)を参照。
+
+起動にはDLLが必要になる。onefileオプションを使用することで、DLLを含めたexe化が可能。
+
+```
+> pyinstaller [ファイル名.py] --onefile
+```
---
+++ patlite_control.py
@@ -0,0 +1,103 @@
+###############################################################################
+# Name    : PATLITE PHE-3B2 serial controller
+# Author  : mokunine
+# Date    : 2022/07/10
+# Version : 0.0.1
+###############################################################################
+
+import sys
+import argparse
+import configparser
+import serial
+
+config = configparser.ConfigParser()
+config.read('config.ini', encoding='utf-8')
+
+parser = argparse.ArgumentParser(description='Serial control.')
+parser.add_argument('-c','--command', type = str, default = '0', choices = ['0', '1'], 
+    help = 'Select a command. 0 = Put off LEDs and stop buzzer. 1 = Put on LEDs and start buzzer.')
+parser.add_argument('-r','--red', type = int, default = 0, choices=[0, 1, 2], 
+    help = 'Select red LED pattern. 0 = none, 1 = on/off, 2 = blink')
+parser.add_argument('-y','--yellow', type = int, default = 0, choices=[0, 1, 2], 
+    help = 'Select yellow LED pattern. 0 = none, 1 = on/off, 2 = blink')
+parser.add_argument('-g','--green', type = int, default = 0, choices=[0, 1, 2], 
+    help = 'Select green led pattern. 0 = none, 1 = on/off, 2 = blink')
+parser.add_argument('-b','--buzzer', type = int, default = 0, choices=[0, 1, 2], 
+    help = 'Select buzzer pattern. 0 = none, 1 = alert, 2 = ringing')
+
+args = parser.parse_args()
+
+# Arguments from ini
+com = "COM" + config['SERIAL']['ComPort']
+baud = config['SERIAL']['Baudrate']
+retry = int(config['SERIAL']['Retry'])
+
+
+def clean():
+    ser.close()
+
+try:
+    ser = serial.Serial(com)
+    ser.baudrate = config['SERIAL']['Baudrate']
+    ser.timeout = int(config['SERIAL']['Timeout'])
+    print(ser)
+except Exception as e:
+    print(e)
+    sys.exit(1) 
+
+headder = ord(config['PHE3FB']['Headder'])
+encode = ord(config['PHE3FB']['Encode'])
+id = [ ord(config['PHE3FB']['Id_high']), ord(config['PHE3FB']['Id_low']) ]
+# All off by default
+data = [ 0x30, 0x30 ] 
+print(args)
+
+command = ord(args.command)
+
+if args.red == 1:
+    data[0] |= 1 << 0
+elif args.red == 2:
+    data[1] |= 1 << 1
+
+if args.yellow == 1:
+    data[0] |= 1 << 2
+elif args.yellow == 2:
+    data[1] |= 1 << 2
+
+if args.green == 1:
+    data[0] |= 1 << 1 
+elif args.green == 2:
+    data[1] |= 1 << 3 
+
+if args.buzzer == 1:
+    data[0] |= 1 << 3
+elif args.buzzer == 2:
+    data[1] |= 1 << 0
+
+# Serial transfer format ( command 0 or 1 )
+tx_data = [ headder, id[1], id[0], command, data[1], data[0], encode ]
+print("Tx data =", tx_data)
+
+
+ser.write( tx_data )
+
+for count in range(retry):
+    result = ser.read_all()
+    if result == b'\x06':
+        print("Transmit success.") 
+        break
+    elif result == b'\x15':
+            print("ERROR - NACK is recieved.") 
+            clean()
+            sys.exit(1)
+    else:
+        if (count + 1) == retry:
+            print("ERROR - ACK wait timeout")
+            clean()
+            sys.exit(1)
+        else:
+            print("retry : " + str(count + 1))
+   
+clean()
+
+sys.exit(0)
